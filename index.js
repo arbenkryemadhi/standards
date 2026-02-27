39d1737e6419808d1eec929918ebb378c25550e6
Arben Kryemadhi <arbenofc@gmail.com>
2026-02-27 17:01:52 -0600

    refactor: update package structure and exports
    - Changed the main entry point from eslint.config.js to index.js.
    - Updated exports to include index.js as the default export.
    - Added a new index.js file that consolidates eslint and prettier configurations.
    - Moved peer dependencies to regular dependencies for better compatibility.
    - Introduced a new script to validate the root export.

---
+++ index.js
@@ -0,0 +1,9 @@
+import eslintConfig from './eslint.config.js';
+import prettierConfig from './prettier.config.js';
+
+export { eslintConfig, prettierConfig };
+
+export default {
+  eslint: eslintConfig,
+  prettier: prettierConfig
+};
